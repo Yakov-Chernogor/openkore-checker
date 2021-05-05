539c78e18f43837bc1a8eed61dc6b95a35060549
Yakov Chernogor <naozumi.t@gmail.com>
2021-05-05 15:23:19 +0300

    fix gh deploy

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/openkore-checker/" : "/",
-  chainWebpack: (config) => {
-    config.module
-      .rule("pegjs")
-      .test(/\.pegjs$/)
-      .use("pegjs-loader")
-      .loader("pegjs-loader")
-      .tap((options) => {
-        return options;
-      })
-      .end();
-  },
+    publicPath: process.env.GH_PAGES === "true" ? "/openkore-checker/" : "/",
+    chainWebpack: config => {
+        config.module
+            .rule("pegjs")
+            .test(/\.pegjs$/)
+            .use("pegjs-loader")
+            .loader("pegjs-loader")
+            .tap(options => {
+                return options;
+            })
+            .end();
+    }
 };
